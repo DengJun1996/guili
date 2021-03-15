6745e157e47a661b7c39ac04287ce1bf79d3e1b3
Administer <371235950@qq.com>
2021-03-15 21:46:11 +0800

    重新规划项目结构

---
+++ service/service-edu/src/main/java/com/atguigu/eduservice/EduApplication.java
@@ -0,0 +1,14 @@
+package com.atguigu.eduservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class EduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EduApplication.class,args);
+    }
+}
